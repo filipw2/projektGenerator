a8b24aee491c8b4092d7ee30cdac364d2da6d1bf
yvalenty <sukrnet@gmail.com>
2017-05-30 20:19:10 +0200

    App Exit + placeholder

--- app/src/main/java/com/example/projekt/generator/MainActivity.java
+++ app/src/main/java/com/example/projekt/generator/MainActivity.java
@@ -79,6 +79,13 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
+        if (id == R.id.exit) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
